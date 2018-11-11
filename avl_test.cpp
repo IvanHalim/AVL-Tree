2db3d5fc920d371438565224405d55f44586f5c0
ivanhalim <ivanhalim@users.noreply.github.com>
2018-11-10 22:29:55 -0800

    Update avl_test.cpp

--- avl_test.cpp
+++ avl_test.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 #include "avl.hpp"
 
+using std::cout;
+using std::endl;
+
 int main() {
     avl myAvl;
+    cout << std::boolalpha << myAvl.isempty() << endl;
     myAvl.insert(48);
     myAvl.insert(32);
     myAvl.insert(40);
@@ -10,6 +14,9 @@ int main() {
     myAvl.insert(64);
     myAvl.remove(32);
     myAvl.insert(4);
+    cout << std::boolalpha << myAvl.isempty() << endl;
+    cout << std::boolalpha << myAvl.contains(96) << endl;
+    cout << std::boolalpha << myAvl.contains(32) << endl;
     myAvl.print();
 
     return 0;
